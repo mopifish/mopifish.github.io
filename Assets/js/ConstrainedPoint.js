4ab4f0c472388292a1b77993fe47acd5e9061986
mopifish <87352493+mopifish@users.noreply.github.com>
2024-08-09 20:19:34 -0700

    Constrained Point

--- Assets/js/ConstrainedPoint.js
+++ Assets/js/ConstrainedPoint.js
@@ -1,4 +1,23 @@
+var constraint_distance = 50
+
+var constraint = new Path.Circle(view.center, constraint_distance);
+constraint.strokeColor = 'black';
+
+var point = new Path.Circle(view.center, 10);
+point.fillColor = 'black';
 
 
-var myCircle = new Path.Circle(new Point(100, 70), 50);
-myCircle.fillColor = 'black';
+function onMouseMove(event){
+	var mouse_pos = event.point;
+
+	constraint.position = mouse_pos;
+
+	point.position = constrain_point(point.position, constraint.position, constraint_distance);
+}
+
+function constrain_point(point, anchor, distance) {
+	var distance = Math.min(point.getDistance(anchor), distance);
+	var result = (point - anchor).normalize() * distance + anchor;
+
+	return result;
+}
